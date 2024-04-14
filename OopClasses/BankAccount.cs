09365688640d72b722dacb68bec97699b52976e3
dell <you@example.com>
2024-04-15 01:04:38 +0530

    Add project files.

---
+++ OopClasses/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopClasses
+{
+    internal class BankAccount
+    {
+        public static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal balence { get; }
+
+        //Constructor
+
+        public BankAccount(string name, decimal initializeBalence)
+        {
+            this.Owner = name;
+            this.balence = initializeBalence;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrowal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ OopClasses/Program.cs
@@ -0,0 +1,8 @@
+﻿using OopClasses;
+
+BankAccount account1 = new BankAccount("Thilina Madushan", 5000);
+BankAccount account2 = new BankAccount("Piyal Siriwardhana", 10000);
+
+
+Console.WriteLine($"This account owner is {account1.Owner} and account number is {account1.Number}");
+Console.WriteLine($"This account owner is {account2.Owner} and account number is {account2.Number}");
